262ba6b7851eb7ef201074c3af7f1ed71045eb4e
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-02-04 13:16:22 +0530

    finished

--- room-homepage/tailwind.config.js
+++ room-homepage/tailwind.config.js
@@ -8,31 +8,15 @@ module.exports = {
         center: true,
 
         // or have default horizontal padding
-        padding: "2rem",
+        // padding: "2rem",
 
         // default breakpoints but with 40px removed
         screens: {
-          sm: "600px",
-          md: "728px",
-          lg: "984px",
-          xl: "1240px",
+          // sm: "600px",
+          // md: "728px",
+          // lg: "984px",
+          // xl: "1240px",
           "2xl": "1496px",
-        },
-        container2: {
-          // you can configure the container to be centered
-          center: true,
-
-          // or have default horizontal padding
-          padding: "1rem",
-
-          // default breakpoints but with 40px removed
-          screens: {
-            sm: "600px",
-            md: "728px",
-            lg: "984px",
-            xl: "1240px",
-            "2xl": "1496px",
-          },
         },
       },
     },
