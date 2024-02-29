24f83e83e3522cd143500c0a4d95fe2c77b73e82
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-02-29 13:43:02 +0530

    renamed

--- loopstudios-landing-page/src/App.jsx
+++ loopstudios-landing-page/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Creations from "./components/Creations";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Feature />
       <Creations />
       <Footer />
+      <Analytics />
     </>
   );
 }
