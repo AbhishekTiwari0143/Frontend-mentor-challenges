da85f52282656d6892ac2d51de40b89dafbebdc7
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-03-04 20:08:44 +0530

    commit

--- Book Store/backend/routes/TodoRoute.js
+++ Book Store/backend/routes/TodoRoute.js
@@ -32,4 +32,55 @@ router.get("/", async (request, response) => {
   }
 });
 
+router.get("/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+
+    const todo = await TodoUser.findById(id);
+
+    if (!todo) {
+      return response.status(400).json({ message: "todo Not Found" });
+    }
+    return response.status(200).send(todo);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(404).send({ message: error.message });
+  }
+});
+
+router.put("/:id", async (request, response) => {
+  try {
+    if (!request.body.title) {
+      return response.status(400).json({ message: "Please Enter Detail!" });
+    }
+    const id = request.params.id;
+
+    const todo = await TodoUser.findByIdAndUpdate(id, request.body);
+
+    if (!todo) {
+      return response.status(400).json({ message: "todo Not Found" });
+    }
+    return response.status(200).send(todo);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+
+    const todo = await TodoUser.findByIdAndDelete(id);
+
+    if (!todo) {
+      return response.status(400).json({ message: "todo Not Found" });
+    }
+    return response.status(200).json({ message: "Deleted" });
+  } catch (error) {
+    console.log(error.message);
+    return response.status(404).send({ message: error.message });
+  }
+});
+
 export default router;
