7d989af320922d46f77f028e05fdaae1afda7417
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-03-19 16:06:06 +0530

    initial commit

---
+++ profile-card-component/src/App.jsx
@@ -0,0 +1,15 @@
+import bottomPattern from "../src/assets/images/bg-pattern-bottom.svg";
+import cardPattern from "../src/assets/images/bg-pattern-top.svg";
+import topPattern from "../src/assets/images/bg-pattern-card.svg";
+
+const App = () => {
+  return (
+    <>
+      <div className="w-full h-screen bg-[--dark-cyan] ">
+        <div className="w-full h-full"></div>
+      </div>
+    </>
+  );
+};
+
+export default App;
