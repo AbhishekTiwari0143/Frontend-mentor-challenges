1a8ef216efb107fe2750b9700b707b69a2933198
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-06-11 17:49:51 +0530

    commit

--- social-proof-section/src/App.jsx
+++ social-proof-section/src/App.jsx
@@ -1,6 +1,8 @@
 import star from "./assets/images/icon-star.svg";
 import Testimonials from "./components/Testimonials";
-// import avtar1 from
+import avtar3 from "./assets/images/image-anne.jpg";
+import avtar1 from "./assets/images/image-colton.jpg";
+import avtar2 from "./assets/images/image-irene.jpg";
 
 function App() {
   return (
@@ -56,8 +58,28 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="testimonials">
-          <Testimonials image={}/>
+        <div className="testimonials flex mt-12 gap-4">
+          <Testimonials
+            image={avtar1}
+            name="Colton Smith"
+            message="We needed the same printed design as the one we had ordered a week prior.
+    Not only did they find the original order, but we also received it in time.
+    Excellent!"
+          />
+          <Testimonials
+            image={avtar2}
+            name="Irene Roberts"
+            message="Customer service is always excellent and very quick turn around. Completely
+    delighted with the simplicity of the purchase and the speed of delivery."
+            top="mt-8"
+          />
+          <Testimonials
+            image={avtar3}
+            name="Anne Wallace"
+            message="Put an order with this company and can only praise them for the very high
+    standard. Will definitely use them again and recommend them to everyone!"
+            top="mt-16"
+          />
         </div>
       </div>
     </>
--- social-proof-section/src/components/Testimonials.jsx
+++ social-proof-section/src/components/Testimonials.jsx
@@ -1,12 +1,16 @@
-const Testimonials = ({ image, name, message }) => {
+const Testimonials = ({ image, name, message, top }) => {
   return (
-    <div className="testimonial-1">
-      <div className="client">
-        <img src={image} alt="image 1" />
-        <h1 className="name">{name}</h1>
-        <h2>Verified Buyer</h2>
+    <div
+      className={`testimonial-1 bg-[--very-dark-magenta] p-6 py-8 rounded-lg ${top} h-max`}
+    >
+      <div className="client flex items-center gap-5">
+        <img src={image} alt="image 1" className="w-10 rounded-[50%]" />
+        <div className="">
+          <h1 className="name text-[--white]">{name}</h1>
+          <h2 className="text-[--soft-pink]">Verified Buyer</h2>
+        </div>
       </div>
-      <p className="">{message}</p>
+      <p className="text-[--white]">{message}</p>
     </div>
   );
 };
