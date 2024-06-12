e59eea3e2d0ad46f737b24aeb0d01db9f65afef5
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-06-12 21:52:59 +0530

    commit

--- social-proof-section/src/App.jsx
+++ social-proof-section/src/App.jsx
@@ -4,35 +4,51 @@ import avtar3 from "./assets/images/image-anne.jpg";
 import avtar1 from "./assets/images/image-colton.jpg";
 import avtar2 from "./assets/images/image-irene.jpg";
 
+import bg_bottom_desktop from "./assets/images/bg-pattern-bottom-desktop.svg";
+import bg_top_desktop from "./assets/images/bg-pattern-top-desktop.svg";
+
+import bg_bottom_mobile from "./assets/images/bg-pattern-bottom-mobile.svg";
+import bg_top_mobile from "./assets/images/bg-pattern-top-mobile.svg";
+
 function App() {
   return (
     <>
-      <div className="container m-auto px-28 pt-24 league-spartan">
-        <div className="upper-part flex justify-between">
-          <div className="texts w-[24rem]">
-            <h1 className="text-5xl font-bold mb-6 text-[--very-dark-magenta]">
+      <div className="container m-auto px-4 py-16 md:px-28 md:pt-24 league-spartan relative">
+        <img
+          src={bg_top_mobile}
+          alt="pattern bottom"
+          className="md:hidden absolute top-0 right left-0 bg-cover"
+        />
+        <img
+          src={bg_top_desktop}
+          alt="pattern bottom"
+          className="hidden md:block absolute top-0 right left-0 bg-cover"
+        />
+        <div className="upper-part flex flex-col md:flex-row justify-between">
+          <div className="texts md:w-[24rem]">
+            <h1 className="text-5xl font-bold mb-6 text-[--very-dark-magenta] text-center md:text-left">
               10,000+ of our users love our products.
             </h1>
-            <p className="text-[--dark-grayish-magenta] font-medium">
+            <p className="text-[--dark-grayish-magenta] font-medium text-center md:text-left">
               We only provide great products combined with excellent customer
               service. See what our satisfied customers are saying about our
               services.
             </p>
           </div>
-          <div className="rating flex flex-col gap-4 w-[600px] mt-4">
-            <div className="bg-[--light-grayish-magenta] font-semibold p-3 flex items-center rounded-lg mr-32 ">
-              <div className="flex gap-2 px-4">
+          <div className="rating flex flex-col gap-4  mt-4 m-auto">
+            <div className="bg-[--light-grayish-magenta] font-semibold p-3 flex flex-col md:flex-row items-center rounded-lg md:w-max pr-16 text-center">
+              <div className="flex gap-2 px-4 mb-2 md:mb-0">
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
               </div>
-              <h1 className="mr-12 ml-6 text-lg text-[--very-dark-magenta] ">
+              <h1 className="mx-6 text-lg text-[--very-dark-magenta] ">
                 Rated 5 Stars in Reviews
               </h1>
             </div>
-            <div className="bg-[--light-grayish-magenta] font-semibold p-3 flex items-center rounded-lg mr-16 ml-14">
+            <div className="bg-[--light-grayish-magenta] font-semibold p-3 flex flex-col md:flex-row items-center rounded-lg w-max md:ml-8">
               <div className="flex gap-2 px-4">
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
@@ -44,8 +60,8 @@ function App() {
                 Rated 5 Stars in Report Guru
               </h1>
             </div>
-            <div className="bg-[--light-grayish-magenta] font-semibold p-3 flex items-center rounded-lg ml-32">
-              <div className="flex gap-2 px-4">
+            <div className="bg-[--light-grayish-magenta] font-semibold p-3 flex items-center flex-col md:flex-row rounded-lg w-max pr-10 md:ml-16">
+              <div className="flex gap-2 px-4 mb-2 md:mb-0">
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
                 <img src={star} alt="star" />
@@ -58,7 +74,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="testimonials flex mt-12 gap-4">
+        <div className="testimonials flex mt-12 gap-4 flex-col md:flex-row">
           <Testimonials
             image={avtar1}
             name="Colton Smith"
@@ -71,16 +87,26 @@ function App() {
             name="Irene Roberts"
             message="Customer service is always excellent and very quick turn around. Completely
     delighted with the simplicity of the purchase and the speed of delivery."
-            top="mt-8"
+            top="md:mt-8"
           />
           <Testimonials
             image={avtar3}
             name="Anne Wallace"
             message="Put an order with this company and can only praise them for the very high
     standard. Will definitely use them again and recommend them to everyone!"
-            top="mt-16"
+            top="md:mt-16"
           />
         </div>
+        <img
+          src={bg_bottom_mobile}
+          alt="pattern bottom"
+          className="absolute md:hidden bottom-0 right-0 bg-cover"
+        />
+        <img
+          src={bg_bottom_desktop}
+          alt="pattern bottom"
+          className="hidden md:block absolute bottom-0 right-0 bg-cover"
+        />
       </div>
     </>
   );
--- social-proof-section/src/components/Testimonials.jsx
+++ social-proof-section/src/components/Testimonials.jsx
@@ -3,14 +3,14 @@ const Testimonials = ({ image, name, message, top }) => {
     <div
       className={`testimonial-1 bg-[--very-dark-magenta] p-6 py-8 rounded-lg ${top} h-max`}
     >
-      <div className="client flex items-center gap-5">
+      <div className="client flex items-center gap-5 mb-5">
         <img src={image} alt="image 1" className="w-10 rounded-[50%]" />
         <div className="">
           <h1 className="name text-[--white]">{name}</h1>
           <h2 className="text-[--soft-pink]">Verified Buyer</h2>
         </div>
       </div>
-      <p className="text-[--white]">{message}</p>
+      <p className="text-[--white]">&ldquo; {message} &rdquo;</p>
     </div>
   );
 };
