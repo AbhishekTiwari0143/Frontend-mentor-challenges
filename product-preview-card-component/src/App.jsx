4caeef0b029c46395b533833b72f41a243fa1c20
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-06-16 19:21:46 +0530

    finished

--- product-preview-card-component/src/App.jsx
+++ product-preview-card-component/src/App.jsx
@@ -6,23 +6,23 @@ function App() {
   return (
     <>
       <div className=" bg-[--Cream] w-full h-screen">
-        <div className="container m-auto flex justify-center items-center h-screen">
-          <div className="box flex w-[550px] bg-[--White] rounded-xl">
-            <div className="left w-[50%]">
+        <div className="container m-auto p-6 md:p-0 flex justify-center items-center md:h-screen">
+          <div className="box flex flex-col md:flex-row md:w-[550px] bg-[--White] rounded-xl shadow-2xl">
+            <div className="left md:w-[50%]">
               <img
                 src={Image}
                 alt="image"
-                className="h-auto object-fit rounded-s-xl"
+                className="h-[20rem] object-cover w-full md:h-auto md:object-fit rounded-t-xl md:rounded-s-xl"
               />
             </div>
-            <div className="right text p-6 flex flex-col gap-4 w-[50%]">
+            <div className="right text p-6 flex flex-col gap-4 md:w-[50%]">
               <h2 className="uppercase tracking-widest text-sm montserrat">
                 Perfume
               </h2>
               <h1 className="text-4xl fraunces font-bold">
                 Gabrielle Essence Eau De Parfum
               </h1>
-              <p className="text-[--Dark-grayish-blue] text-sm font-medium montserrat">
+              <p className="text-[--Dark-grayish-blue] text-[14px] font-medium montserrat">
                 A floral, solar and voluptuous interpretation composed by
                 Olivier Polge, Perfumer-Creator for the House of CHANEL.
               </p>
@@ -32,7 +32,7 @@ function App() {
                 </h1>
                 <p className="line-through">$169.99</p>
               </div>
-              <div className="flex bg-[--Dark-cyan] p-3 rounded-xl justify-center gap-2 items-center">
+              <div className="flex bg-[--Dark-cyan] p-3 rounded-xl justify-center gap-2 items-center hover:bg-[--Very-dark-blue]">
                 <img src={Cart} alt="image cart" className="" />
                 <h1 className="text-[--White]  font-bold text-sm montserrat">
                   Add to Cart
